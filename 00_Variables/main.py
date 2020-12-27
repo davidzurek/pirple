6ed1c1674fbbe59433da501af4272c7646892315
David Z <54494933+davidzurek@users.noreply.github.com>
2020-12-27 23:15:28 +0100

    Add files via upload

---
+++ 00_Variables/main.py
@@ -0,0 +1,59 @@
+# This file contains a list of song attributes as well as parts of its lyrics.
+
+# Song information
+title = "Make It Better"
+artist = "Anderson Paak"
+featured_artist = "Smokey Robinson"
+genre = "R&B/Soul"
+duration_sec = 220
+release_year = 2019
+
+# Additional information about album, label, producer and price
+album = "Ventura"
+label = "Aftermatch Entertainment & 12Tone Music"
+written_by = "Anderson Paak"
+amazon_music_price = 1.29
+itunes_price = 1.29
+
+# First verse and chorus of the song
+"""
+Verse 1: Anderson .Paak]
+How do you mend when you're worlds apart?
+Ooh, carry on
+Ooh, from the start
+Somehow, we fell in love, then fell right out of touch
+And ooh, that was hard
+But ooh, here we are
+And it’s easier to walk away
+Than to look for what would make you stay
+Remember when we first met?
+We were havin' so much fun
+So, now, how can we both forget
+Tellin' each other, "We’re the one"?
+We would make love (We would make love)
+At the drop of a hat
+Remember that? (Yeah)
+I remember you and me (You and me)
+Close as any two can be (Two can be)
+Now we're strangers in the night (In the night)
+Awkward and uptight
+
+[Chorus: Anderson .Paak & Both]
+Oh baby, do you wanna make it better?
+Uh, ha, do you wanna stay together?
+Hey, if you do, then let's please
+Make some new memories
+"""
+
+
+print(title)
+print(artist)
+print(featured_artist)
+print(genre)
+print(duration_sec)
+print(release_year)
+print(album)
+print(label)
+print(written_by)
+print(amazon_music_price)
+print(itunes_price)
